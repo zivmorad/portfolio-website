f9af49a555d3452fe5152885b20dce88680a23ea
moradziv <moradziv1994@gmail.com>
2020-05-16 14:22:07 +0300

    fix initial

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+
+const App=()=>{
+    return(
+        <div>
+            {/* <Navbar/> */}
+            app
+        </div>
+    )
+}
+
+
+export default App;
