aa3b24d0c0e96a5d88d15ee84fdb555532908946
moradziv <moradziv1994@gmail.com>
2020-06-07 02:29:31 +0300

    fix css

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -7,7 +7,7 @@ const Intro=()=>{
         <div id='intro'>
             <div className='intro-container'>
                 <img className='intro-img' src={zivPic} alt='error'/>
-                <h1 className='intro-title'>Hi, I am <strong> Ziv Morad</strong></h1>
+                <h1 className='intro-title'>Hi, I am <p> Ziv Morad</p></h1>
                 <p className='intro-subtitle'>Front-End Developer</p>
             </div>
         </div>
